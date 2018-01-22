14d6dda13d936da9782b3647e820874f67dd62fd
Stefan Beckers <stefan.beckers206490@student.leeuwenborgh.nl>
2018-01-22 16:29:50 +0100

    Add project files.

---
+++ engine_v2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace engine_v2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Frame MainMenu = new Frame();
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainScreen.Children.Add(MainMenu);
+            MainMenu.NavigationUIVisibility = NavigationUIVisibility.Hidden;
+            MainMenu.HorizontalAlignment = HorizontalAlignment.Stretch;
+            MainMenu.VerticalAlignment = VerticalAlignment.Stretch;
+            MainMenu.Navigate(new details());
+        }
+
+        private void Line_Click(object sender, RoutedEventArgs e)
+        {
+            Line myLine = new Line();
+            myLine.Stroke = Brushes.Black;
+            myLine.X1 = 1;
+            myLine.X2 = 250;
+            myLine.Y1 = Convert.ToInt64(MainScreen.Height) - 1;
+            myLine.Y2 = Convert.ToInt64(MainScreen.Height) - 150;
+            //myLine.HorizontalAlignment = HorizontalAlignment.Left;
+            //myLine.VerticalAlignment = VerticalAlignment.Bottom;
+            myLine.StrokeThickness = 5;
+            MainScreen.Children.Add(myLine);
+        }
+    }
+}
---
+++ engine_v2/details.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace engine_v2
+{
+    /// <summary>
+    /// Interaction logic for details.xaml
+    /// </summary>
+    public partial class details : Page
+    {
+        public details()
+        {
+            //comment
+            InitializeComponent();
+            MaterialName.SelectionMode = SelectionMode.Single;
+            MaterialName.Items.Add("chroom");
+            MaterialName.Items.Add("goud");
+            MaterialName.Items.Add("ijzer");
+            MaterialName.SelectedItem = "ijzer";
+        }
+
+        private void MaterialName_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (MaterialName.SelectedItem.ToString() == "chroom")
+            {
+                Dichtheid.Content = 7190;
+            }
+            else if (MaterialName.SelectedItem.ToString() == "ijzer")
+            {
+                Dichtheid.Content = 7870;
+            }
+            else if (MaterialName.SelectedItem.ToString() == "goud")
+            {
+                Dichtheid.Content = 19300;
+            }
+
+        }
+
+        private void Mass_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            BerekenFz();
+        }
+        public void BerekenFz()
+        {
+            Zwaartekracht.Content = (Convert.ToDouble(Valversnelling.Text) * Convert.ToDouble(Mass.Text)).ToString();
+        }
+    }
+}
